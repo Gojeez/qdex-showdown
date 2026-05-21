e3e29b3c509f90dc610fffeb78ff08c4b08704d8
sheepy <tkemert20@gmail.com>
2026-05-21 12:10:54 -0400

    fix

--- server/sockets.ts
+++ server/sockets.ts
@@ -340,7 +340,7 @@ export class ServerStream extends Streams.ObjectReadWriteStream<string> {
 			const roomidRegex = /^\/(?:[A-Za-z0-9][A-Za-z0-9-]*)\/?$/;
 			const cssServer = new StaticServer('./config');
 			const avatarServer = new StaticServer('./config/avatars');
-			const staticServer = new StaticServer('./server/static');
+			const staticServer = new StaticServer('../qdex-showdown-client/play.pokemonshowdown.com', {indexFile: 'qc-client.html'});
 			const staticRequestHandler = (req: http.IncomingMessage, res: http.ServerResponse) => {
 				// console.log(`static rq: ${req.socket.remoteAddress}:${req.socket.remotePort} -> ${req.socket.localAddress}:${req.socket.localPort} - ${req.method} ${req.url} ${req.httpVersion} - ${req.rawHeaders.join('|')}`);
 				req.resume();
